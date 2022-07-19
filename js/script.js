a5bb38a5babfc7b27d7de3a419522672d27c7881
Anastasia211282 <mazajka1982@gmail.com>
2022-07-19 21:50:16 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// const vechicleBodyWidth = 5000,
+//     vechicleBodyLength = 4000;
+
+// console.log("Ширина кузова авто: " + vechicleBodyWidth + ', длина кузова авто: ' + vechicleBodyLength);
+
+//cnake_case
+//UPPER_SNAKE_CASE
+//Kebab-case
+//PascalCase
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+//console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[0]);
+
+// const storeName = 'My awesome store';
+
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Alex', 'John', 'Sam'],
+//     products: {
+//         'soap': 20,
+//         'water': 5
+//     },
+//     open: true
+// }
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your sername?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log(incr);
+    console.log(decr);
+
+const isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
